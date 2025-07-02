bfca2ac50c93066817df81e5ad4a286eeea2f802
Robert Porchas <robertgeomaing@gmail.com>
2025-07-02 16:31:59 -0700

    Add files via upload

---
+++ mainB.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Robert's Intro Page</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<?php /*the original code had an if statement like this
+// if ($mode === "dark"): class = "dark"<?php endif?> */?>
+<body class="dark">
+    <h1>What's below is a series of mini/medium sized projects I will be working on in php</h1>
+    <p>These projects are random ideas I have had or found, regardless if they are finished they will be up on here.</p>
+    <ul>
+        <li><a href="../firstProj/css/index.php">First Project</a> - Basic page that was my first attempt at creating.</li>
+        <li><a href="../secondProj/proj.php">Second Project</a> - A calculator that performs basic arithmetic operations.</li>
+    </ul>
+    <h1>Some projects will be nonpages, like scripts or functions <br>that I will be working on of which will be here.</h1>
+
+
+</body>
+</html>
